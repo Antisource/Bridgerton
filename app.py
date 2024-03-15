671837939e6877f4dec40d2e11755f61c9283816
Rishab Pendalwar <70773782+Antisource@users.noreply.github.com>
2024-03-15 19:42:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import email,smtplib,ssl
 from PIL import Image
 
-json_file = open('model.json'),'r')
+json_file = open('model.json','r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model=model_from_json(loaded_model_json)
