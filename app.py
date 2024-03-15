cb43be3aef2cad367f6d577ce5b03cfb3ad28ee6
Rishab Pendalwar <70773782+Antisource@users.noreply.github.com>
2024-03-15 19:41:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-import streamlit as streamlit
+import streamlit as st
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-
+import numpy as np
 import email,smtplib,ssl
 from PIL import Image
 
-json_file = open('model.json','r')
+json_file = open('model.json'),'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model=model_from_json(loaded_model_json)
@@ -15,15 +15,16 @@ loaded_model.load_weights("model.h5")
 with open('./bridgerton-quotes.txt','r',encoding='utf-8') as file:
     data =file.read()
 
+
 def generate_seq(model,tokenizer,max_length,seed_text,n_words):
-    in_text=seed_text
+    in_text seed_text
 
     for i in range(in_words):
         encoded=tokenizer.texts_tosequences([in_text])[0]
 
         encoded=pad_sequences([encoded],maxlen=max_length, padding='pre')
-
-        yhat=model.predict_classes(encode,verbose=0)
+        predict_x=model.predict(encoded)
+        yhat=np.argmax(predict_x,axis=1)
         out_word=''
 
         for word,index in tokenizer.word_index.items():
@@ -41,7 +42,7 @@ st.title('Bridgeton Text Generator SMS Web App')
 
 image=Image.open('./1.jpg')
 
-st.image(image,use_column_width=True)
+st.image(image)
 
 st.write("This is a Bridgeton Quotes Text Generator Web App")
 
@@ -53,14 +54,16 @@ generate_word = st.button("Generate Text Words")
 
 send_sms = st.button("Send Text Messages")
 
+
+
 def send_sms_via_email(number,message,provider,sender_credentials,subject,smpt_server='smpt.gmai.com',smpt_port=465):
     sender_email,email_password=sender+sender_credentials
     receiver_email=f'{number}@{provider}'
 
     email_message=f"Subject:{subject}\nTo:{receiver_email}\n{message}"
-    
+
 if s_text and n  and generate_word:
-    st.header(generate_seq(loaded_model,tokenizer,max_length-1,s_text,n))
+    st.header(generate_seq(loaded_model,tokenizer,max_length-1,s_text,n)).
 
 elif s_text and generate_word and not n:
     st.write('Please input number of words')
